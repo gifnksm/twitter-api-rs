458b5aba06a56e2d38873b0a82c3e354fdc9142f
gifnksm <makoto.nksm+github@gmail.com>
2015-01-19 22:05:52 +0900

    fix some warnings

--- examples/tweet.rs
+++ examples/tweet.rs
@@ -2,8 +2,6 @@
         unused, unused_extern_crates, unused_import_braces,
         unused_qualifications, unused_results, unused_typecasts)]
 
-#![feature(old_orphan_check)]
-
 extern crate "twitter-api" as twitter;
 extern crate "rustc-serialize" as rustc_serialize;
 extern crate "oauth-client" as oauth;
