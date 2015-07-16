39406a2ddf7c7c5350bf9fa4bf73d3b00e167cfd
k0pernicus <antonin.carette@gmail.com>
2015-07-16 20:34:50 +0200

    src/lib.rs : Differenciation between api software and api oauth

--- src/lib.rs
+++ src/lib.rs
@@ -10,10 +10,13 @@ use std::borrow::{Cow, IntoCow};
 use std::collections::HashMap;
 use oauth::Token;
 
-mod api {
+mod api_twitter_oauth {
     pub const REQUEST_TOKEN: &'static str   = "https://api.twitter.com/oauth/request_token";
     pub const AUTHORIZE: &'static str       = "https://api.twitter.com/oauth/authorize";
     pub const ACCESS_TOKEN: &'static str    = "https://api.twitter.com/oauth/access_token";
+}
+
+mod api_twitter_soft {
     pub const STATUSES_UPDATE: &'static str = "https://api.twitter.com/1.1/statuses/update.json";
 }
 
@@ -29,20 +32,20 @@ fn split_query<'a>(query: &'a str) -> HashMap<Cow<'a, str>, Cow<'a, str>> {
 }
 
 pub fn get_request_token(consumer: &Token) -> Token<'static> {
-    let resp = oauth::get(api::REQUEST_TOKEN, consumer, None, None);
+    let resp = oauth::get(api_twitter_oauth::REQUEST_TOKEN, consumer, None, None);
     let param = split_query(&resp);
     Token::new(param.get("oauth_token").unwrap().to_string(),
                param.get("oauth_token_secret").unwrap().to_string())
 }
 
 pub fn get_authorize_url(request: &Token) -> String {
-    format!("{}?oauth_token={}", api::AUTHORIZE, request.key)
+    format!("{}?oauth_token={}", api_twitter_oauth::AUTHORIZE, request.key)
 }
 
 pub fn get_access_token(consumer: &Token, request: &Token, pin: &str) -> Token<'static> {
     let mut param = HashMap::new();
     let _ = param.insert("oauth_verifier".into_cow(), pin.into_cow());
-    let resp = oauth::get(api::ACCESS_TOKEN, consumer, Some(request), Some(&param));
+    let resp = oauth::get(api_twitter_oauth::ACCESS_TOKEN, consumer, Some(request), Some(&param));
     let param = split_query(&resp);
     Token::new(param.get("oauth_token").unwrap().to_string(),
                param.get("oauth_token_secret").unwrap().to_string())
@@ -53,5 +56,6 @@ pub fn get_access_token(consumer: &Token, request: &Token, pin: &str) -> Token<'
 pub fn update_status(consumer: &Token, access: &Token, status: &str) {
     let mut param = HashMap::new();
     let _ = param.insert("status".into_cow(), status.into_cow());
-    let _ = oauth::post(api::STATUSES_UPDATE, consumer, Some(access), Some(&param));
+    let _ = oauth::post(api_twitter_soft::STATUSES_UPDATE, consumer, Some(access), Some(&param));
+}
 }
